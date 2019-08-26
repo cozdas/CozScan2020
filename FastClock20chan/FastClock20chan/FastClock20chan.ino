2cc37344d75b31e34019942839f03815804c876f
cozdas <cozdas@2aa104b0-7ad8-4a1c-af81-c1113692ed93>
2019-08-26 09:26:45 +0000

    - reworked the logic in the strobe call to have the system more robust. - Added sanity checks to strobe making sure that not two channels are connected to a single bus - if no channels on Bus B are turned on, we now let the bus B to dangle (connected to either input nor sense). This reduces the possible leaks between bus a and bus b channels.

--- FastClock20chan/FastClock20chan/FastClock20chan.ino
+++ FastClock20chan/FastClock20chan/FastClock20chan.ino
@@ -43,7 +43,7 @@
 /* SCAN CHANNEL TO PIN MAPPING
 chan DPin   NAME  PORT    BIT   MicroPin
 0     --, 
-1     15    PB1   PORTB   1     17
+1     15    PB1   PORTB   1     17  B:1320 D:32104 C:6 
 2     14    PB3   PORTB   3     16
 3     16    PB2   PORTB   2     18
 4     17    PB0   PORTB   0     19
@@ -53,7 +53,8 @@ chan DPin   NAME  PORT    BIT   MicroPin
 8     3     PD0   PORTD   0     25
 9     4     PD4   PORTD   4     26
 10    5     PC6   PORTC   6     27
-11    21    PF4   PORTF   4     7
+
+11    21    PF4   PORTF   4     7   B:7654 C: D:67 E:6 F:410 
 12    22    PF1   PORTF   1     8
 13    23    PF0   PORTF   0     9
 14    12    PD6   PORTD   6     34
@@ -63,6 +64,7 @@ chan DPin   NAME  PORT    BIT   MicroPin
 18    8     PB4   PORTB   4     30
 19    7     PE6   PORTE   6     29
 20    6     PD7   PORTD   7     28
+
 21    13    PC7   PORTC   7     1
 22    30    PD5   PORTD   5     (11: wire from TX led)
 */
@@ -98,6 +100,12 @@ chan DPin   NAME  PORT    BIT   MicroPin
 //temp variables to hold register set/clear bits
 uint8_t _PB, _PC, _PD, _PE, _PF;
 
+const uint8_t PortMaskB = 0xFF; //8
+const uint8_t PortMaskC = 0xC0; //2
+const uint8_t PortMaskD = 0xFF; //8
+const uint8_t PortMaskE = 0x40; //1 
+const uint8_t PortMaskF = 0x13; //3 = total 22 bits
+
 //mapping from scanner control bits to MCU registers (2 bits per entry, first bit clear, second set)
 uint8_t* controlbit2port[]    = {&_PF,&_PF,&_PF,&_PD,&_PB,&_PB,&_PB,&_PB,&_PE,&_PD,&_PB,&_PB,&_PB,&_PB,&_PD,&_PD,&_PD,&_PD,&_PD,&_PC,&_PC};
 uint8_t  controlbit2portbit[] = {   4,   1,   0,   6,   7,   6,   5,   4,   6,   7,   1,   3,   2,   0,   3,   2,   1,   0,   4,   6,   7};
@@ -112,6 +120,8 @@ uint8_t   numBitsCollected = 0;
 
 void TestRun()
 {
+  delay(5000);
+
   for(;;)
   {
     //reset chan 21, this will connect bus_b to intput
@@ -137,7 +147,30 @@ void TestRun()
   }
 }
 
+//return true if internal relay states makes sense
+bool SanityCheck(uint32_t busA, uint32_t busB )
+{
+  //check if busses have more than 1 bit set  
+  bool tooManyBitsInA = busA & (busA-1); //this op clears the rightmost 1 bit. if result is nonzero, then more than one bit was set 
+  bool tooManyBitsInB = busB & (busB-1); //this op clears the rightmost 1 bit. if result is nonzero, then more than one bit was set 
 
+  if(tooManyBitsInA | tooManyBitsInB)
+    return false;
+    
+  bool busBtoSense  = _PC&(1<<7);
+  bool busBtoIn     = _PD&(1<<5);
+
+  //bus B should be connected to either In or sense
+  if(busBtoSense && busBtoIn)
+    return false;
+
+  //now make sure that if both busA and busB has connected channels, then the busB should not connect to input (4-wire should be on)
+  if(busA && busB && !busBtoSense)
+    return false;
+
+  //passed all checks
+  return true;
+}
 
 //we process the bits in two passes to guarantee the "break before connect" ordering.
 //pass1: read the odd bits, which are reset coil commands, and clear the corresponding port bits for those channels
@@ -159,50 +192,65 @@ void strobe()
   }
   */
 
+  //disconnect everything
+  PORTB &= ~PortMaskB;
+  PORTC &= ~PortMaskC;
+  PORTD &= ~PortMaskD;
+  PORTE &= ~PortMaskE;
+  PORTF &= ~PortMaskF;  
 
   //Pass0: process the reset bits (even bits)
   {
-    _PB = _PC = _PD = _PE = _PF = 0;
     uint64_t st = state;
-    //uint32_t st = states[0];
+    for(int i=0; i<21; ++i, st>>=2)
+    {
+      (*controlbit2port[i]) &= ~((st&1)<<(controlbit2portbit[i]));
+    }
+
+    //also if the SENSE connection is set, break the INPUT connection now
+    //if(state&(1ll<<41))
+    //  PORTD &= ~(1<<5);
+  }
+
+  //Pass1:  process the set bits (odd bits)
+  {
+    uint64_t st = state>>1;
     for(int i=0; i<21; ++i, st>>=2)
     {
       (*controlbit2port[i]) |= (st&1)<<(controlbit2portbit[i]);
     }
 
-    PORTB &= ~_PB;
-    PORTC &= ~_PC;
-    PORTD &= ~_PD;
-    PORTE &= ~_PE;
-    PORTF &= ~_PF;  
+    //BusB2In = !BusB2Sense (PD5 = !PC7)
+    //_PD = (PD&~(1<<5)) | (((~_PC)&(1<<7))>>2);
+    if(_PC&(1<<7))
+      _PD &= ~(1<<5);
+    else 
+      _PD |= (1<<5);
 
-    //also if the SENSE connection is set, break the INPUT connection now
-    if(state&(1ll<<41))
-      PORTD &= ~(1<<5);
+    //arbitrarily arranged bitmap of each bus for bit counting (busA: chan 1..10, busB: chan 11..20)
+    uint32_t busA = (_PB & 0x0Fl) | ((_PC & 0x40l)<<8) | ((_PD & 0x1Fl)<<16);                                            //chans 1..10
+    uint32_t busB = (_PB & 0xF0l) |                     ((_PD & 0xC0l)<<8)  | ((_PE & 0x40l)<<16) | ((_PF & 0x13l)<<24);   //chans 11..20
+
+    if(SanityCheck(busA, busB))
+    {   
+      PORTB |= _PB;
+      PORTE |= _PE;
+      PORTF |= _PF;
+
+      //if no channel is active on BusB, then let BusB dangle (reduces leakage between BusA and BusB channels)
+      if(busB)
+      {
+        PORTC |= _PC;
+        PORTD |= _PD;
+      }
+      else
+      {
+        PORTC |= (_PC & ~(1<<7));
+        PORTD |= (_PD & ~(1<<5));       
+      }
+    }
   }
-
-  //Pass1:  process the set bits (odd bits)
-  {
-
-    _PB = _PC = _PD = _PE = _PF = 0;
-    uint64_t st = state>>1;
-    //uint32_t st = states[1];
-    for(int i=0; i<21; ++i, st>>=2)
-    {
-      (*controlbit2port[i]) |= (st&1)<<(controlbit2portbit[i]);
-    } 
-
-    PORTB |= _PB;
-    PORTC |= _PC;
-    PORTD |= _PD;
-    PORTE |= _PE;
-    PORTF |= _PF;
-  } 
-
-  //also if the SENSE connection was broken, sat the INPUT connection now
-  if(state&(1ll<<40))
-    PORTD |= (1<<5);
-
+  
   state = 0;  
   numBitsCollected = 0;
 }
@@ -222,10 +270,13 @@ void setup()
   pinMode(19, INPUT); //A1: PF6: CLOCK
   pinMode(20, INPUT); //A2: PF5: STROBE
 
+  //port states
+  _PB = _PC = _PD = _PE = _PF = 0;
+
   state = 0;  
   numBitsCollected = 0;
 
-  //  TestRun();
+  //TestRun();
 }
 
 //active waiting the pin states is much faster than interrupt driven system
